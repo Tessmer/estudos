eb1d9f938121298f25a4bded484a04742babf9eb
Samanta Tessmer <samantartessmer@hotmail.com>
2023-05-02 17:02:17 -0300

    feat: redux

---
+++ store/configureStore.js
@@ -0,0 +1,11 @@
+import Redux from "redux";
+import counter from "./counter.js";
+
+const reducer = Redux.combineReducers({ counter });
+
+const store = Redux.createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
