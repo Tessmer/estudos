1f71869f803b4f43218f72b2892d022631825788
Samanta Tessmer <samantartessmer@hotmail.com>
2023-05-09 12:23:06 -0300

    feat: adding react redux filter

---
+++ react-redux2/react-redux2/src/App.js
@@ -0,0 +1,15 @@
+import Filter from "./components/Filter";
+import Products from "./components/Products";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Filter />
+      <Products />
+    </div>
+  );
+}
+
+export default App;
